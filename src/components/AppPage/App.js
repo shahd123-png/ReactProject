ac38a8c84bec89a2e0f4e1e9a62c31eb12db396c
Shahd Dawod <71906350+shahd123-png@users.noreply.github.com>
2023-11-08 14:44:48 +0200

    Add files via upload

---
+++ src/components/AppPage/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useTheme } from '@mui/material/styles';
+import { Container, Button } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+    <Button variant="contained" >
+    Custom Color Button
+  </Button>
+    </Container>
+   
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
-import reportWebVitals from './Tests/reportWebVitals';
+import App from './components/AppPage/App';
+import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: 
+    {
+      main: 'rgba(147, 125, 194, 1)', 
+    },
+
+    secondary: 
+    {
+      main: 'rgba(147, 125, 194, 0.6)', 
+    },
+
+    customColor: {
+      main: 'rgba(198, 137, 198, 1)', 
+    },
+  },
+
+
+  typography: {
+    h1:{
+      fontFamily: 'Inter',
+      fontSize: 48,
+    },
+    p:{
+      fontFamily: 'Inter',
+      fontSize: 16,
+      fontWeight: 400
+    }
+
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <ThemeProvider theme={theme}>
+  
+  <App />
+  
+  </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
