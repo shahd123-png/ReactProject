1febe1e6a212d86d1aca00435695a45e1245be94
Shahd Dawod <71906350+shahd123-png@users.noreply.github.com>
2023-11-13 12:27:26 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,36 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/AppPage/App';
-import reportWebVitals from './reportWebVitals';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 const theme = createTheme({
   palette: {
-    primary: 
+    primary:
     {
-      main: 'rgba(147, 125, 194, 1)', 
+      main: 'rgba(147, 125, 194, 1)',
     },
 
-    secondary: 
+    secondary:
     {
-      main: 'rgba(147, 125, 194, 0.6)', 
+      main: 'rgba(147, 125, 194, 0.6)',
     },
 
     customColor: {
-      main: 'rgba(198, 137, 198, 1)', 
+      main: 'rgba(198, 137, 198, 1)',
     },
+    
+
   },
 
 
   typography: {
-    h1:{
+    h1: {
       fontFamily: 'Inter',
       fontSize: 48,
+      fontWeight: 700
     },
-    p:{
+    p: {
       fontFamily: 'Inter',
       fontSize: 16,
       fontWeight: 400
+    },
+    h2: {
+      fontFamily: 'Inter',
+      fontSize: '38px',
+      fontStyle: 'normal',
+      fontWeight: '600',
+      lineHeight: '49px'
+    },
+
+    h3: {
+      fontFamily: 'Inter',
+      fontSize: 24,
+      fontWeight: 600
+    },
+
+    h4: {
+      fontFamily: 'Inter',
+      fontSize: 22,
+      fontWeight: 550
+    }
+    ,
+    h5: {
+      fontSize: '18px',
+      fontS: 'normal',
+      fontWeight: '400'
     }
 
   },
@@ -40,12 +66,9 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
-  
-  <App />
-  
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-reportWebVitals();
